baad27acbdad34575248490ce421c774920a7876
Alessandro Margara <alessandro.margara@polimi.it>
2022-08-25 10:58:25 +0200

    Added script to convert from cromlech to pangaea input.

--- pangaea/pangaea-artefact/input_converter/cromlech_pangaea_input_converter.py
+++ pangaea/pangaea-artefact/input_converter/cromlech_pangaea_input_converter.py
@@ -1,5 +1,5 @@
+import yaml
 import sys
-import json
 
 # Process relations for a single entity
 def process_entity_relations(entity, out_file):
@@ -54,41 +54,74 @@ def process_operation(operation, out_file):
     name = operation['name']
     out_file.write(' * name: ' + name + '\n')
         
-    consistency = operation['consistency']
+    consistency = 'low' if operation['consistency'] == 'L' else 'high'
     out_file.write(' * integrity: ' + consistency + '\n')
 
     frequency = operation['frequency']
     out_file.write(' * frequency: ' + str(frequency) + '\n')
 
-    forced_entity = operation['forced_entity']
     out_file.write(' * forced_entities:\n')
-    out_file.write(' *   - ' + forced_entity + '\n')
+    out_file.write(' *   - ### TODO va bene lasciare sempre vuoto?\n')
 
     db_accesses = operation['database_access']
     process_db_accesses(db_accesses, out_file)
 
     out_file.write(' */\n\n')
+
+# Peocess a single entity of an operation
+def process_entity(op_entity, entities):
+    attributes = set()
+    if 'read_attributes' in op_entity.keys():
+        for attr in op_entity['read_attributes']:
+            attributes.add(attr)
+    if 'write_attributes' in op_entity.keys():
+        for attr in op_entity['write_attributes']:
+            attributes.add(attr)
+
+    name = op_entity['entity_name']
+    if name in entities.keys():
+        entities[name].update(attributes)
+    else:
+        entities[name] = attributes
+
+# Print entities to file
+def print_entities(entities, out_file):
+    for e in entities.keys():
+        out_file.write('/**\n')
+        out_file.write(' * @Entity\n')
+        out_file.write(' * name: ' + e + '\n')
+        out_file.write(' * implementation: ' + e + '\n')
+        out_file.write(' * replication_overhead: 1 ### TODO possiamo cambiare valore?\n')
+        out_file.write(' * columns:\n')
+        for attr in entities[e]:
+            out_file.write(' *   - ' + attr + '\n')
+        out_file.write(' * relations:\n')
+        out_file.write(' *   - entity1_name: ### TODO come determinare?\n')
+        out_file.write(' *     type: ### TODO come determinare?\n')
+        out_file.write(' */\n\n')
     
 # Process the list of entities
-def process_entities(json_data, out_file):
-    for entity in json_data['entities']:
-        process_entity(entity, out_file)
+def process_entities(yaml_data, out_file):
+    entities = {}
+    for operation in yaml_data['operations']:
+        for op_entity in operation['database_access']:
+            process_entity(op_entity, entities)
 
+    print_entities(entities, out_file)
+        
 # Process the list of operations
-def process_operations(json_data, out_file):
-    for operation in json_data['operations']:
+def process_operations(yaml_data, out_file):
+    for operation in yaml_data['operations']:
         process_operation(operation, out_file)
 
 if __name__ == "__main__":
-    input_path = sys.argv[1] if len(sys.argv) > 1 else 'input.json'
+    input_path = sys.argv[1] if len(sys.argv) > 1 else 'input.yaml'
     input_file = open(input_path, 'r')
-    json_data = json.load(input_file)
+    yaml_data = yaml.safe_load(input_file)
     input_file.close()
 
     output_path = sys.argv[2] if len(sys.argv) > 2 else 'output.txt'
     output_file = open(output_path, 'w')
-    process_entities(json_data, output_file)
-    process_operations(json_data, output_file)
+    process_entities(yaml_data, output_file)
+    process_operations(yaml_data, output_file)
     output_file.close()
-
-
