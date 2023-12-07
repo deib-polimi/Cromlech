0e96aa1792c01054ce7790d72b6334e3db16dd4f
Alessandro Margara <alessandro.margara@polimi.it>
2023-12-07 10:32:58 +0100

    Added a new file that evaluates the costs of cromlech solutions.

--- evaluate_cromlech_solution.py
+++ evaluate_cromlech_solution.py
@@ -484,10 +484,10 @@ def post_processing_communication_cost(result):
                 for o in ops:
                     if attributes_iton.get(a) in op_writes.get(o):
                         wop += nodes_dict.get(o).get_frequency()
-    #print("POST PROCESSING:")
-    #print("WOP " + str(wop))
-    #print("ROP " + str(rop))
-    #print("REPL " + str(repl))
+    print("POST PROCESSING:")
+    print("WOP " + str(wop))
+    print("ROP " + str(rop))
+    print("REPL " + str(repl))
     return wop + rop + repl
 
 # Removes all columns which only create noise (as they are only accessed by a single operation) and returns
@@ -1460,58 +1460,6 @@ def optimizer(op_num, max_com_cost, num_services, alpha):
     #print(post_processing_communication_cost(opt_result))
     format_and_draw_final(opt_result, input_file + '_M-' + str(num_services) + '_alpha-' + str(alpha) + '.html')
 
-def run_experiment(alpha, num_services):
-    print("--- " + input_file + " --- num services=" + str(num_services) + " --- alpha=" + str(alpha) + " ---") 
-    write_dat_file(num_services)
-    optimizer(op_num, max_com_cost, num_services, alpha)
-    
-if __name__ == '__main__':
-    # Input file (without .yaml extension)
-    input_file = sys.argv[1]
-    # Maximum number of services
-    num_services = int(sys.argv[2])
-    # Alpha (between 0 and 1)
-    alpha = float(sys.argv[3])
-    # Timeout (seconds)
-    timeout = int(sys.argv[4]) 
-    parse_arch_yaml(input_file)
-    to_remove = noise_removal()
-    graph_copy = Graph()
-    op_num = len(graph)
-    trim_operations(to_remove)
-    op_num = len(graph)
-    attributes = build_attr_relationships()
-    build_hcw_relationships()
-    clean_names()
-    services = force_operations()
-    bound_ops = {}
-    for s in services:
-        for o1 in s:
-            for o2 in graph:
-                if o2 in s:
-                    bound_ops.update({(o1, o2): 1})
-                else:
-                    bound_ops.update({(o1, o2): 0})
-
-    for l in services:
-        attrs = []
-        for s in l:
-            attrs += op_writes.get(s) + op_reads.get(s)
-        attrs = list(set(attrs))
-        for a in attrs:
-            services[services.index(l)].append(attributes_ntoi.get(a))
-
-    identify_static_attributes()
-    identify_hc_readonly_attributes()
-    elect_primary_replicas(services)
-
-    min_decoupling_bound = compute_total_coupling([[o for o in graph]])
-    max_decoupling_bound = compute_total_coupling(services)
-    max_com_cost = compute_communication_cost(services)
-    
-    run_experiment(alpha, num_services)
-
-
 def setup(input_file):
     global to_remove, graph_copy, op_num, attributes, bound_ops, services, buond_ops, services, attrs, min_decoupling_bound, max_decoupling_bound, max_com_cost
     parse_arch_yaml(input_file)
@@ -1551,10 +1499,32 @@ def setup(input_file):
     return max_com_cost
 
 def evaluate(cromlech_arch, results, alpha=0.5):
+    results_only_nums = []
+    for m in results:
+        ops = [x for x in m if isinstance(x, int)]
+        attrs = [x for x in m if isinstance(x, str)]
+        attrs_num = [int(x[:len(x) - 1]) for x in attrs]
+        results_only_nums.append(ops + attrs_num)
+
     output = {}
     max_com_cost = setup(cromlech_arch)
-    output["cohesion"] = compute_total_coupling(results)
+    output["cohesion"] = compute_total_coupling(results_only_nums)
     communication_cost = post_processing_communication_cost(results)
     output["normalized_communication_cost"] = communication_cost / max_com_cost
+    print("Communication cost: " + str(communication_cost / max_com_cost))
     output["total_value"] = alpha*output["cohesion"] - (1-alpha)*output["normalized_communication_cost"]
+    
     return output
+    
+if __name__ == '__main__':
+    # Input file (without .yaml extension)
+    input_file = sys.argv[1]
+
+    # Alpha
+    alpha = float(sys.argv[2])
+    
+    # Add here the results to evaluate
+    results = [[7, 27, 33, 34, 35, 36, 38, 41, 46, 47, 48, 51, '100011N', '100020N', '100023N', '100025P', '100026P', '100027P', '100028P', '100029P', '100030P', '100059P', '100069P', '100073P', '100074P', '100075P', '100076P', '100077P', '100078P', '100079P', '100080P', '100087P', '100088P', '100089P', '100090P', '100091P', '100092P', '100093P', '100094P', '100102P', '100103P', '100104P', '100105P', '100106P', '100107P', '100108P', '100109P', '100110P', '100111P', '100112P', '100113P', '100114P', '100115P', '100116P', '100117P', '100118P', '100119P', '100120P', '100122P', '100128R', '100129R', '100130N', '100141P', '100142P', '100143P', '100144P', '100155N'], [10, 17, 20, 28, 45, 53, 54, 65, '100000P', '100001P', '100002P', '100003P', '100004P', '100005P', '100006P', '100007P', '100008P', '100009P', '100010P', '100011P', '100012P', '100013P', '100014P', '100015P', '100016P', '100017P', '100018P', '100019P', '100020P', '100021P', '100032R', '100033R', '100034R', '100035R', '100036R', '100037R', '100038R', '100045N', '100054N', '100055N', '100056N', '100072P', '100074R'], [6, 8, 37, 42, 50, '100025R', '100026R', '100029R', '100070P', '100071P', '100095P', '100131P', '100132P', '100134N'], [12, 16, 57, 58, '100060P', '100061P', '100062P', '100063P', '100064P', '100065P', '100066P', '100067P', '100068P'], [13, 18, 39, 40, 59, '100081P', '100082P', '100083P', '100084P', '100085P', '100086P', '100135P', '100136P', '100137P'], [43, 44, 14, 19, 62, '100096P', '100097P', '100098P', '100099P', '100100P', '100101P', '100138P', '100139P', '100140P'], [0, 1, 2, 3, 4, 5, 9, 11, 15, 21, 22, 23, 24, 25, 26, 29, 30, 31, 32, 49, 52, 55, 56, 60, 61, 63, 64, 66, 67, 68, '100022P', '100023P', '100024P', '100031P', '100032P', '100033P', '100034P', '100035P', '100036P', '100037P', '100038P', '100039P', '100040P', '100041P', '100042P', '100043P', '100044P', '100045P', '100046P', '100047P', '100048P', '100049P', '100050P', '100051P', '100052P', '100053P', '100054P', '100055P', '100056P', '100057P', '100058P', '100059R', '100069R', '100070R', '100071R', '100081R', '100082R', '100083R', '100084R', '100085R', '100086R', '100094R', '100096R', '100097R', '100098R', '100099R', '100100R', '100101R', '100121P', '100123P', '100124P', '100125P', '100126P', '100127P', '100128P', '100129P', '100130P', '100131N', '100132R', '100133P', '100134P', '100135R', '100136R', '100137R', '100138R', '100139R', '100140R', '100144R', '100145P', '100146P', '100147P', '100148P', '100149P', '100150P', '100151P', '100152P', '100153P', '100154P', '100155P', '100156P', '100157P', '100158P', '100159P', '100160P', '100161P', '100162P', '100163P', '100164P', '100165P']]
+    
+    output = evaluate(input_file, results, alpha)
+    print(output)
